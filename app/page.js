aadd1451bcdbf93580eb61e143655e168e4b56dd
Bhaswanth <BhaswanthRM@gmail.com>
2025-05-19 21:53:15 +0530

    remove comments

--- app/page.js
+++ app/page.js
@@ -9,34 +9,16 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
 
-// -----------------Used to get and process Blog data from dev-folio------------------
-
-// async function getData() {
-//   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-//   if (!res.ok) {
-//     throw new Error('Failed to fetch data')
-//   }
-
-//   const data = await res.json();
-
-//   const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-//   return filtered;
-// };
-
 export default async function Home() {
   // const blogs = await getData();
 
   return (
     <div suppressHydrationWarning >
       <HeroSection />
-      <AboutSection />
-      {/* <Experience /> */}
+      <AboutSection />      
       <Skills />
       <Projects />
-      <Education />
-      {/* <Blog blogs={blogs} /> */}
+      <Education />      
       <ContactSection />
     </div>
   )
