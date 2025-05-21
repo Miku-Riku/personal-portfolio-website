a59d8853333f225e0860776156c7f24e190e5a3c
Bhaswanth <BhaswanthRM@gmail.com>
2025-05-21 18:07:47 +0530

    update about me decription

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,9 @@ export const personalData = {
   name: "V Bhaswanth",
   profile: '/profile.jpeg',
   designation: "Web and Software Developer",
-  description: "I am a Full Stack Developer, I am fascinated about new technologies and very curious and interested to learn and explre and get skilled. And I am a quick learner always looking for new challenges.",
+  description: `A highly motivated Computer Science and Engineering graduate from the class of 2024, I am eager to leverage my academic foundation in software development and problem-solving. Throughout my B.Tech program, I have cultivated a strong understanding of algorithms, data structures, and programming languages. I am keen to apply my technical skills and analytical abilities to contribute effectively to a dynamic and innovative environment.
+Also I actively work on personal skill development of popular technologies like popular web dev tech stack, Data Analysis, AI/ML etc.
+`,
   email: 'bhaswanth5555@gmail.com',
   phone: '',
   address: 'Andhra Pradesh, India',
