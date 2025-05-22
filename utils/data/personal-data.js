4a316c5121d6510799956178722db03d0cbfd059
Bhaswanth <BhaswanthRM@gmail.com>
2025-05-22 12:31:53 +0530

    add line breaks in personal.description

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "V Bhaswanth",
   profile: '/profile.jpeg',
   designation: "Web and Software Developer",
-  description: `A highly motivated Computer Science and Engineering graduate from the class of 2024, I am eager to leverage my academic foundation in software development and problem-solving. Throughout my B.Tech program, I have cultivated a strong understanding of algorithms, data structures, and programming languages. I am keen to apply my technical skills and analytical abilities to contribute effectively to a dynamic and innovative environment.
+  description: `A highly motivated Computer Science and Engineering graduate from the class of 2024, I am eager to leverage my academic foundation in software development and problem-solving. \nThroughout my B.Tech program, I have cultivated a strong understanding of algorithms, data structures, and programming languages. \nI am keen to apply my technical skills and analytical abilities to contribute effectively to a dynamic and innovative environment.
 Also I actively work on personal skill development of popular technologies like popular web dev tech stack, Data Analysis, AI/ML etc.
 `,
   email: 'bhaswanth5555@gmail.com',
